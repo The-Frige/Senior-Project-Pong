db0cf23201541c04cbe879687d89610a14a82af5
spacexkid <muskox799@gmail.com>
2022-02-25 22:00:21 -0800

    Add files via upload

---
+++ src/App.cpp
@@ -0,0 +1,80 @@
+#include "App.h"
+
+App::GameState App::gamestate = Multiplayer;
+App::App() : mWindow(sf::VideoMode(800, 600), "Pong")
+{
+	mWindow.setFramerateLimit(60);
+}
+
+void App::run()
+{
+	while (mWindow.isOpen())
+	{
+		processWindowEvents();
+		player1.move();
+		player2.move();
+		ball.move(player1, player2);
+		render();
+	}
+}
+
+void App::processWindowEvents()
+{
+	sf::Event event;
+	while (mWindow.pollEvent(event))
+	{
+		switch(event.type)
+		{
+		case sf::Event::Closed:
+			mWindow.close();
+			break;
+		case sf::Event::KeyPressed:
+			if(event.key.code == sf::Keyboard::Escape)
+			{
+				mWindow.close();
+			}
+			//handleInput(event.key.code);
+			break;
+		}
+	}
+}
+
+/* not needed
+void App::handleInput(sf::Keyboard::Key key)
+{
+	if (key == sf::Keyboard::Escape)
+	{
+		mWindow.close();
+	}
+	/*
+	else if (key == sf::Keyboard::E)
+	{
+		gamestate = App::Multiplayer;
+	}
+	else if (key == sf::Keyboard::P)
+	{
+		gamestate = App::NotStarted;
+	}
+
+}
+*/
+
+void App::render()
+{
+	if(gamestate == App::Multiplayer)
+	{
+		mWindow.clear(sf::Color::Black);
+		player1.show(mWindow);
+		player2.show(mWindow); //Disable if gamestate is set to singleplayer
+		ball.show(mWindow);
+		mWindow.display();
+	}
+	//Debug code to see if gamestates are working
+	/*
+	else if(gamestate == App::NotStarted)
+	{
+		mWindow.clear();
+		mWindow.display();
+	}
+	*/
+}
---
+++ src/Main.cpp
@@ -0,0 +1,9 @@
+#include <SFML/Graphics.hpp>
+#include "App.h"
+
+int main()
+{
+	App app;
+	app.run();
+    return 0;
+}
